8eac7872675f6257a8692f280a409d41ff484a8c
JINOOK JUNG <jinook929@gmail.com>
2020-11-08 03:52:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ function moveDodgerLeft() {
   }
 }
 
-function moveDodgerRight() {
-  let leftNumbers = dodger.style.left.replace('px', '');
-  let left = parseInt(leftNumbers, 10);
+// function moveDodgerRight() {
+//   let leftNumbers = dodger.style.left.replace('px', '');
+//   let left = parseInt(leftNumbers, 10);
  
-  if (left < 360) {
+//   if (left < 360) {
+//     dodger.style.left = `${left + 1}px`;
+//   }
+// }
+
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.right.replace('px', '');
+  let right = parseInt(rightNumbers, 10);
+ 
+  if (right > 0) {
     dodger.style.left = `${left + 1}px`;
   }
 }
